d8e24a047fe925171b5ad137c15197c225fdaf12
Anirudh Mahato <120658927+AnirudhMahato@users.noreply.github.com>
2025-11-17 11:59:51 +0530

    Refactor TrustSync contract with updated syntax

--- contracts/TrustSync.sol
+++ contracts/TrustSync.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 /**
  * @title TrustSync
- * @dev A decentralized trust and reputation system for collaborative agreements.
+ * @dev A decentralized trust and reputation system for collaborative agreements
  */
 contract Project {
     
@@ -178,3 +178,4 @@ contract Project {
     }
 
 }
+
