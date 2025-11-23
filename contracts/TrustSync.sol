7977aa7a0139d3981ac49c6325b18a00cf3d2828
Anirudh Mahato <120658927+AnirudhMahato@users.noreply.github.com>
2025-11-23 19:29:55 +0530

    Fix issues in TrustSync contract

--- contracts/TrustSync.sol
+++ contracts/TrustSync.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 /**
  * @title TrustSync 
- * @dev A decentralized trust and reputation system for collaborative agreements
+ * @dev A decentralized trust and reputation system for collaborative agreements.
  */
 contract Project {
     
@@ -178,6 +178,7 @@ contract Project {
     }
 
 }
+
 
 
 
